fb0f94b9eeaca80efc80a8429cb7d16283cae084
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2022-11-13 23:34:44 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import React from "react";
+import Search from "./components/search";
+import Footer from "./components/footer";
+// import BasicFn from "./components/rating";
+function App() {
+  return (
+    <>
+      <Search />;
+      <Footer />
+    </>
+  );
+}
+
+export default App;
